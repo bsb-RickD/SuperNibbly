321b2929bf252abbd41c173fcb798adde5e2146f
bsb-RickD <bsb.RickD@gmail.com>
2022-12-21 22:13:17 +0100

    Preparations for enabling the worker loop

--- intro.asm
+++ intro.asm
@@ -43,7 +43,7 @@ palfade_in:
 
 .macro commands_to_add p1, p2, p3, p4
 .if .paramcount = 0
-   .byte 0,0,0
+   .byte 0,0,0,0
 .elseif .paramcount = 1
    .byte p1,0,0,0
 .elseif .paramcount = 2
@@ -58,17 +58,31 @@ palfade_in:
 .define no_commands_to_add commands_to_add
 
 function_ptrs:
-ptr_check_return_to_basic:                ; 0 - check for exit
+   .word 0,0                              ; 0 - nullptr
+   no_commands_to_add
+ptr_check_return_to_basic:                ; 1 - check for exit
    .word check_return_to_basic, 0         
    no_commands_to_add
 
-ptr_animate_fish:                         ; 1 - fish animation
+ptr_check_fill_screen:                    ; 2 - unpack screen data
+   .word fill_screen, 0         
+   commands_to_add 3                      ; after unpacking, wait for fadeout
+
+ptr_check_fade_out_done:                  ; 3 - wait for fading out the palette
+   .word fade_out_and_switch_to_tiled_mode, palfade_out         
+   commands_to_add 4,5,6                  ; after fading out, fade back in and start the animations
+
+ptr_fade_in:                              ; 4 - fade pal in
+   .word fade_intro_in, palfade_in
+   no_commands_to_add
+
+ptr_animate_smoke:                        ; 5 - smoke animation
+   .word animate_sprite, animated_smoke
+   no_commands_to_add
+ptr_animate_fish:                         ; 6 - fish animation
    .word animate_sprite, jumping_fish     
    no_commands_to_add
 
-ptr_animate_smoke:                        ; 2 - smoke animation
-   .word animate_sprite, animated_smoke
-   no_commands_to_add
 
 work_queue:
    .res 64,0
@@ -86,14 +100,16 @@ return_to_basic:
 .proc main
    
    ; restore state for multiple runs
-   LoadB palfade_state, 0
-   LoadB return_to_basic, 0
-   LoadB work_queue, 0
-   LoadB workers_to_add, 0
-   LoadB workers_to_remove, 0
+   stz palfade_state
+   stz palfade_in+5
+   stz palfade_out+5
+   stz return_to_basic
+   stz work_queue
+   stz workers_to_add
+   stz workers_to_remove
 
    LoadW R15, work_queue
-   lda #0
+   lda #1
    jsr array_append              ; append check for exit to worker queue
 
    init_vsync_irq initial_fade_out   
@@ -119,7 +135,6 @@ show_screen:
 
    ; switch to the intro screen - but it's still "invisible" because it's all a single color
    jsr switch_to_tiled_mode
-
 
    ; initialize pal fade
    LoadW R15, palfade_in
@@ -156,31 +171,45 @@ repeat:
    ldx #1
 fetch_next_worker:
    pha                           ; push the number of workers to call
+   phx                           ; save worker index
+
    lda work_queue,x              ; get next function number
    asln 3                        ; multiply by 8
-   tay
-   ; load address to jump to and write it to jsr below
-   lda function_ptrs,y
-   sta jsr_to_patch+1
-   iny 
-   lda function_ptrs,y
-   sta jsr_to_patch+2
-   ; load this pointer and copy it to R15
-   iny
-   lda function_ptrs,y
-   sta R15L
-   iny
-   lda function_ptrs,y
-   sta R15H
-   ; save counter
-   inx
-   phx
-jsr_to_patch:   
-   jsr $DEAD                     ; dispatch the call
-   plx
-   pla
+   tay                           ; y holds the offset to the function_ptrs table
+
+   jsr call_worker               ; call the worker
+
+   ; carry clear? - nothing to do, call worker again next frame
+   bcc call_worker_next_frame    
+
+   ; carry is set - this means remove current worker and add the new workers
+   LoadW R15, workers_to_add
+   ldx #4                        ; max of 4 pseudo pointers to add
+append_workers:   
+   iny                           ; advance to next worker to load
+   lda function_ptrs,y           ; get the 1 byte pseudo pointer
+   beq no_more_workers_2_append  ; null ptr found - stop evaluation workers to add
+   phx                           ; remember x, y
+   phy
+   jsr array_append              ; add it to the list of pointers to add 
+   ply
+   plx                           ; restore x,y
+   dex                           ; dec the counter of allowed pointers to add
+   bne append_workers            ; try one more
+no_more_workers_2_append:
+   LoadW R15, workers_to_remove
+   pla                           ; pull worker index - this is the one we want to remove
+   pha                           ; push worker index back onto the stack
+   jsr array_append              ; add the current
+call_worker_next_frame:   
+   plx                           ; restore worker index   
+   inx                           ; advance to next index
+   pla                           ; restore count
    dec
-   bne fetch_next_worker
+   bne fetch_next_worker         ; not at zero? get next worker
+
+   jsr remove_expired_workers    ; update the workers list by removing old..
+   jsr add_new_workers           ; and adding new workers
 
    lda return_to_basic
    beq repeat
@@ -195,6 +224,110 @@ work_loop_empty:
    rts
 .endproc
 
+
+; check on the palette fade out  to be complete..
+.proc fade_out_and_switch_to_tiled_mode
+   ldy #5
+   lda (R15),y          ; get fade state - 0 means fade is complete    
+   beq complete
+   clc                  ; indicate that worker is not done yet
+   rts
+complete:
+
+   ; set all used colors to fade target
+   ldx #(PALETTE_SIZE/2)-1
+   lda #0
+   MoveW palfade_out+3, R11
+   jsr write_to_palette_const_color
+
+   ; switch to the intro screen - but it's still "invisible" because it's all a single color
+   jsr switch_to_tiled_mode
+
+   sec                  ; fade complete - carry on with new workers
+   rts   
+.endproc
+
+.proc fade_intro_in
+   ; are we here the first time?
+   ldy #5
+   lda (R15),y
+   bne fade_further
+
+   ; initialize pal fade
+   LoadW R0, fadebuffer
+   sec
+   jsr palettefader_start_fade
+   bra write_the_pal
+
+fade_further:
+   ; second time round, fade..
+   LoadW R0, fadebuffer
+   jsr palettefader_step_fade
+
+write_the_pal:
+   MoveW R0, R11 
+   ldx #(PALETTE_SIZE/2)-1
+   lda #0
+   jsr write_to_palette
+
+   ldy #5
+   lda (R15),y
+   beq complete                  ; are we done?
+
+   clc                           ; fade not done, carry on
+   rts                  
+complete:
+   sec                           ; indicate that we're done
+   rts
+.endproc
+
+; call the worker 
+; pass this pointer in R15
+;
+; y: index to function_ptrs table (worker index * 8)
+;
+; a,x get thrashed, y is pushed/pulled
+;
+; return: 
+;  C = 0: worker not done, call again next frame
+;  C = 1: worker has completed its work
+;
+.proc call_worker
+   ; load address to jump to and write it to jsr below
+   lda function_ptrs,y
+   sta jsr_to_patch+1
+   iny 
+   lda function_ptrs,y
+   sta jsr_to_patch+2
+   ; load this pointer and copy it to R15
+   iny
+   lda function_ptrs,y
+   sta R15L
+   iny
+   lda function_ptrs,y
+   sta R15H      
+   phy                           ; save index to worker data
+jsr_to_patch:   
+   jsr $DEAD                     ; dispatch the call
+   ply
+
+   rts
+.endproc
+
+
+; take all workers from workers_to_remove and remove them from work_queue
+.proc remove_expired_workers
+   stz workers_to_remove         ; mark list as empty
+   rts
+.endproc
+
+; take all workers from workers_to_add and add them to work_queue
+.proc add_new_workers
+   stz workers_to_add            ; mark list as empty
+   rts
+.endproc
+
+
 .proc check_return_to_basic
    jsr KRNL_GETIN    ; read key
    cmp #KEY_Q         
@@ -202,13 +335,15 @@ work_loop_empty:
    ; q pressed - signal that we want to return to basic
    lda #1
    sta return_to_basic
-carry_on:   
+carry_on:  
+   clc 
    rts
 .endproc   
 
 
 palfade_state:
-   .byte 0
+.byte 0
+
 .proc initial_fade_out
    jsr push_current_vera_address
    lda #%00001111                   ; R0,R1,R2,R3
@@ -311,12 +446,15 @@ done:
    rts
 .endproc
 
+; uncompress the screen data 
+; plus some sprites
 .proc fill_screen
    ; vera address0 set to 0, increment 1
    set_vera_address 0   
    LoadW R0, screen           ; screendata to R0 (source)        
    LoadW R1, VERA_data0       ; vera data #0 to R1 (destination)
    jsr memory_decompress
+   sec                        ; set carry flag to indicate that the next worker task should start
    rts   
 .endproc
 
