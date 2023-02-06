bdb83bb4e63a15a320ee31f75df66d0114b718fc
bsb-RickD <bsb.RickD@gmail.com>
2023-02-06 14:52:57 +0100

    Made code more concise, using macros

--- lib/work_queue.asm
+++ lib/work_queue.asm
@@ -109,22 +109,8 @@ work_loop_empty:
 ;  C = 1: worker has completed its work
 ;
 .proc call_worker
-   ;ldy #0   
    ThisLoadW R0, jsr_to_patch+1, 0     ; load address to jump to and write it to jsr below
    ThisLoadW R0, R15, -                ; load this pointer for call into R15
-   /*
-   lda (R0),y
-   sta jsr_to_patch+1
-   iny 
-   lda (R0),y
-   sta jsr_to_patch+2
-   ; load this pointer and copy it to R15
-   iny
-   lda (R0),y
-   sta R15L
-   iny
-   lda (R0),y
-   sta R15H      */
    PushW R0                            ; save R0
 jsr_to_patch:   
    jsr $CA11                           ; dispatch the call
